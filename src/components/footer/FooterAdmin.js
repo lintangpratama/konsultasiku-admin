009781627394a4628e4af412b02b52c1ff3e266d
Lintang Aji Yoga Pratama <lintangajiyogapratama@gmail.com>
2023-05-19 19:24:44 +0700

    update

--- src/components/footer/FooterAdmin.js
+++ src/components/footer/FooterAdmin.js
@@ -37,7 +37,7 @@ export default function Footer() {
         {" "}
         &copy; {1900 + new Date().getYear()}
         <Text as="span" fontWeight="500" ms="4px">
-          Konsultasiku
+          Jobspot
         </Text>
       </Text>
     </Flex>
